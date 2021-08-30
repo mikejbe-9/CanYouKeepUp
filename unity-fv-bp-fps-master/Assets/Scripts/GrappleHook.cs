f0fd6aaa6610b35ef96438fde599ce1a974cefd2
mikejbe-9 <68225257+mikejbe-9@users.noreply.github.com>
2021-08-31 01:06:32 +0530

    Game mechanics

---
+++ unity-fv-bp-fps-master/Assets/Scripts/GrappleHook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleHook : MonoBehaviour
+{
+    [SerializeField] private float swingingSpeed = 70f;
+    [SerializeField] private float length = 20f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(GameConstants.k_ButtonNameAction))
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out RaycastHit rayCastHit, length))
+            {
+
+            }
+        }
+    }
+}
---
+++ unity-fv-bp-fps-master/Assets/Scripts/UI/CrosshairManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairManager : MonoBehaviour
+{
+    public Image crosshairImage;
+    public Sprite nullCrosshairSprite;
+    public float crosshairUpdateshrpness = 5f;
+
+    PlayerWeaponsManager m_WeaponsManager;
+    bool m_WasPointingAtEnemy;
+    bool m_WasPointingAtHookable;
+    RectTransform m_CrosshairRectTransform;
+    CrosshairData m_CrosshairDataDefault;
+    CrosshairData m_CrosshairDataTarget;
+    CrosshairData m_CrosshairDataGrapple;
+    CrosshairData m_CurrentCrosshair;
+
+    void Start()
+    {
+        m_WeaponsManager = GameObject.FindObjectOfType<PlayerWeaponsManager>();
+        DebugUtility.HandleErrorIfNullFindObject<PlayerWeaponsManager, CrosshairManager>(m_WeaponsManager, this);
+
+        OnWeaponChanged(m_WeaponsManager.GetActiveWeapon());
+
+        m_WeaponsManager.onSwitchedToWeapon += OnWeaponChanged;
+    }
+
+    void Update()
+    {
+        UpdateCrosshairPointingAtEnemy(false);
+        UpdateCrossHairPointingAtHookable(false);
+        m_WasPointingAtEnemy = m_WeaponsManager.isPointingAtEnemy;
+        m_WasPointingAtHookable = m_WeaponsManager.isPointingAtHookable;
+    }
+
+    void UpdateCrosshairPointingAtEnemy(bool force)
+    {
+        if (m_CrosshairDataDefault.crosshairSprite == null)
+            return;
+
+        if ((force || !m_WasPointingAtEnemy) && m_WeaponsManager.isPointingAtEnemy)
+        {
+            m_CurrentCrosshair = m_CrosshairDataTarget;
+            crosshairImage.sprite = m_CurrentCrosshair.crosshairSprite;
+            m_CrosshairRectTransform.sizeDelta = m_CurrentCrosshair.crosshairSize * Vector2.one;
+        }
+        else if ((force || m_WasPointingAtEnemy) && !m_WeaponsManager.isPointingAtEnemy)
+        {
+            m_CurrentCrosshair = m_CrosshairDataDefault;
+            crosshairImage.sprite = m_CurrentCrosshair.crosshairSprite;
+            m_CrosshairRectTransform.sizeDelta = m_CurrentCrosshair.crosshairSize * Vector2.one;
+        }
+
+        crosshairImage.color = Color.Lerp(crosshairImage.color, m_CurrentCrosshair.crosshairColor, Time.deltaTime * crosshairUpdateshrpness);
+
+        m_CrosshairRectTransform.sizeDelta = Mathf.Lerp(m_CrosshairRectTransform.sizeDelta.x,
+             m_CurrentCrosshair.crosshairSize, Time.deltaTime * crosshairUpdateshrpness) * Vector2.one;
+    }
+
+    void UpdateCrossHairPointingAtHookable(bool force)
+    {
+        if ((force || !m_WasPointingAtHookable) && m_WeaponsManager.isPointingAtHookable)
+        {
+            Debug.Log("IS POINTING AT HOOKABLE");
+            m_CurrentCrosshair = m_CrosshairDataGrapple;
+            crosshairImage.sprite = m_CurrentCrosshair.crosshairSprite;
+            //m_CrosshairRectTransform.sizeDelta = m_CurrentCrosshair.crosshairSize * Vector2.one;
+        }
+        else if ((force || m_WasPointingAtHookable) && !m_WeaponsManager.isPointingAtHookable)
+        {
+            m_CurrentCrosshair = m_CrosshairDataDefault;
+            crosshairImage.sprite = m_CurrentCrosshair.crosshairSprite;
+            //m_CrosshairRectTransform.sizeDelta = m_CurrentCrosshair.crosshairSize * Vector2.one;
+        }
+
+        crosshairImage.color = Color.Lerp(crosshairImage.color, m_CurrentCrosshair.crosshairColor, Time.deltaTime * crosshairUpdateshrpness);
+
+        //m_CrosshairRectTransform.sizeDelta = Mathf.Lerp(m_CrosshairRectTransform.sizeDelta.x,
+        //     m_CurrentCrosshair.crosshairSize, Time.deltaTime * crosshairUpdateshrpness) * Vector2.one;
+    }
+
+    void OnWeaponChanged(WeaponController newWeapon)
+    {
+        if(newWeapon)
+        {
+            crosshairImage.enabled = true;
+            m_CrosshairDataDefault = newWeapon.crosshairDataDefault;
+            m_CrosshairDataTarget = newWeapon.crosshairDataTargetInSight;
+            m_CrosshairDataGrapple = newWeapon.crosshairDataHookableInSight;
+            m_CrosshairRectTransform = crosshairImage.GetComponent<RectTransform>();
+            DebugUtility.HandleErrorIfNullGetComponent<RectTransform, CrosshairManager>(m_CrosshairRectTransform, this, crosshairImage.gameObject);
+        }
+        else
+        {
+            if (nullCrosshairSprite)
+            {
+                crosshairImage.sprite = nullCrosshairSprite;
+            }
+            else
+            {
+                crosshairImage.enabled = false;
+            }
+        }
+
+        UpdateCrosshairPointingAtEnemy(true);
+        UpdateCrossHairPointingAtHookable(true);
+    }
+}
