4d57504538134311867293c9956e784df5486e12
mikejbe-9 <68225257+mikejbe-9@users.noreply.github.com>
2021-09-01 20:45:10 +0530

    time rewind

---
+++ unity-fv-bp-fps-master/Assets/Scripts/PointInTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointInTime
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public float healthPoints;
+
+    public PointInTime(Vector3 pos, Quaternion rot, float hp)
+    {
+        position = pos;
+        rotation = rot;
+        healthPoints = hp;
+    }
+}
--- unity-fv-bp-fps-master/Assets/Scripts/TimeRewind.cs
+++ unity-fv-bp-fps-master/Assets/Scripts/TimeRewind.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class TimeRewind : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isRewinding;
+    public float recordTime = 5f;
+
+    private List<PointInTime> pointsInTime;
+    private Health health;
+
     void Start()
     {
-        
+        pointsInTime = new List<PointInTime>();
+        health = GetComponent<Health>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartRewind();
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            StopRewind();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+
+    void Record()
+    {
+        if (pointsInTime.Count > Mathf.Round( recordTime / Time.fixedDeltaTime))
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
+        pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation, health.currentHealth));
+    }
+
+    void Rewind()
+    {
+        PointInTime point = pointsInTime[0];
+        transform.position = point.position;
+        transform.rotation = point.rotation;
+        health.currentHealth = point.healthPoints;
+        pointsInTime.RemoveAt(0);
+    }
+
+    void StartRewind()
+    {
+        isRewinding = true;
+    }
+
+    void StopRewind()
+    {
+        isRewinding = false;
     }
 }
